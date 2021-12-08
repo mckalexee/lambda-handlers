a26f5a0cbe4fc2912ac698948a7e8d148571332d
Alex McKenzie <alex@unbreak-it.com>
2021-12-08 16:36:05 -0500

    add test launcher

--- lib/parse-handlers.test.ts
+++ lib/parse-handlers.test.ts
@@ -2,10 +2,15 @@ import * as path from 'path';
 import { parseHandlers } from './parse-handlers';
 
 describe('Parse Handlers', () => {
-	test('Handlers are parsed', () => {
-		const parsedHandlers = parseHandlers(
+	test('Handlers are parsed', async () => {
+		const parsedHandlers = await parseHandlers(
 			path.join(__dirname, '../test-handlers/'),
 		);
+
+		Object.entries(parsedHandlers.api).forEach(([path, definition]) => {
+			expect(path.endsWith('test-api.handler.ts')).toBeTruthy();
+			expect(definition.memorySize).toBe(512);
+		});
 	});
 });
 
